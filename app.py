1939fb01e3bb5f2ee854b3a2e731bba3c1e2624f
gabrielnov <gabrielcarvalho169@hotmail.com>
2025-03-23 19:49:09 -0300

    adiciona relatorios

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 from faker import Faker
+import plotly.express as px
+from datetime import datetime
 
 # Interface Streamlit
 def main():
@@ -26,7 +28,7 @@ def main():
         st.subheader("Contas a Receber")
         df = pd.read_sql_query("SELECT * FROM contas_receber", conn)
         st.dataframe(df)
-        
+    
     elif choice == "Lançamentos":
         st.subheader("Lançamentos Financeiros")
         df = pd.read_sql_query("SELECT * FROM lancamentos", conn)
@@ -36,7 +38,58 @@ def main():
         st.subheader("Relatório de Fluxo de Caixa")
         df = pd.read_sql_query("SELECT tipo, SUM(valor) as total FROM lancamentos GROUP BY tipo", conn)
         st.dataframe(df)
+
+        st.subheader("Distribuição das Contas a Pagar por Fornecedor")
+        df = pd.read_sql_query("SELECT * FROM contas_pagar", conn)
+
+        df_grouped = df.groupby('fornecedor', as_index=False)['valor'].sum()
+        df_grouped = df_grouped.sort_values(by='valor', ascending=False)
+        fig = px.pie(df_grouped, names='fornecedor', values='valor', title='Distribuição das Contas a Pagar')
+        st.plotly_chart(fig)
     
+        st.subheader("Top 5 Clientes com Maior Receita")
+        query =  """SELECT c.nome AS Cliente, SUM(cr.valor) as total_receita
+        FROM contas_receber cr
+        JOIN clientes c ON cr.cliente_id = c.id
+        WHERE cr.status = 'Recebido'
+        GROUP BY cr.cliente_id
+        ORDER BY total_receita DESC
+        LIMIT 5"""
+        df = pd.read_sql_query(query, conn)
+        fig = px.bar(df, x="Cliente", y="total_receita", text_auto=True,
+                     labels={"Receita": "Total (R$)", "Cliente": "Clientes"},
+                     title="Top 5 Clientes com Maior Receita")
+        
+        st.plotly_chart(fig)
+
+        st.subheader("Comparação Receita vs Despesa (Gráfico de Barras)")
+
+        current_month = datetime.now().strftime('%Y-%m')
+    
+        cursor.execute("""
+            SELECT SUM(valor) FROM lancamentos 
+            WHERE tipo = 'Receita' AND strftime('%Y-%m', data) = ?
+        """, (current_month,))
+        receita = cursor.fetchone()[0] or 0
+        
+        cursor.execute("""
+            SELECT SUM(valor) FROM lancamentos 
+            WHERE tipo = 'Despesa' AND strftime('%Y-%m', data) = ?
+        """, (current_month,))
+        despesa = cursor.fetchone()[0] or 0
+        
+        df_comparacao = pd.DataFrame({
+            "Categoria": ["Receita", "Despesa"],
+            "Valor": [receita, despesa]
+        })
+        
+        fig_comparacao = px.bar(df_comparacao, x="Categoria", y="Valor", text_auto=True,
+                                labels={"Valor": "Total (R$)", "Categoria": "Tipo"},
+                                title="Receita vs Despesa do Mês Atual")
+        
+        st.plotly_chart(fig_comparacao)
+
+
     conn.close()
     
 if __name__ == "__main__":
